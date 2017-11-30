03d3cc57fdef9bac7bd833e90ebb13b0de9de6df
Sandeep Singh <sandha.iitr@gmail.com>
2017-11-30 11:26:58 -0800

    update

--- client/SendData.py
+++ client/SendData.py
@@ -1,3 +1,6 @@
+#cleaning up the Hbase Database: note it will delete all the tables
+#echo 'list.each {|t| disable t; drop t}; quit;' | ./bin/hbase shell
+
 # importing the requests library
 import requests
  
@@ -6,12 +9,8 @@ API_ENDPOINT = "http://localhost:8080/api/data"
 
 # data to be sent to api
 data = "{\"data\":[{\"uuid\":\"xxxx\",\"timestamp\":1499813708623,\"value\":15,\"geometryType\":\"point\",\"coordinates\":[[30,60]]}]}"
-#length = len(data)
-#print(length)
-#headers = {'content-type': 'application/json','Content-Lenght':'110'}
-# sending post request and saving response as response object
-#r = requests.post(url = API_ENDPOINT, data = data,headers=headers)
 
+# Sending Data
 r = requests.post(url = API_ENDPOINT, data = data)
 
 # extracting response text 
@@ -24,7 +23,7 @@ API_ENDPOINT_QUERY = "http://localhost:8080/api/querydata"
 
 
 #query to be send to api
-query="{\"query\":{\"lat_min\":10,\"lat_max\":90,\"lng_min\":10,\"lng_max\":90,\"timestamp_min\":1499813608623,\"timestamp_max\":1499813808623}}"
+query="{\"query\":{\"lat_min\":29,\"lat_max\":31,\"lng_min\":59,\"lng_max\":61,\"timestamp_min\":1499813608623,\"timestamp_max\":1499813808623}}"
 
 r = requests.post(url = API_ENDPOINT_QUERY, data = query)
 
