fad8c0377fc7b16346b8f090ddd512df532ef19b
Sandeep Singh <sandha.iitr@gmail.com>
2017-11-30 19:54:09 -0800

    update

--- client/RegisterSensor.py
+++ client/RegisterSensor.py
@@ -10,15 +10,29 @@
 import requests
  
 # defining the api-endpoint 
-API_ENDPOINT = "http://localhost:8080/api/point"
+API_ENDPOINT = "https://localhost:8080/api/point"
 
-sensor = "{\"query\":{\"pointType\":\"temp\",\"unit\":\"F\"}}"
+sensor = "{\"query\":{\"userToken\":\"e1e7013a-fb31-4087-8019-8d210396867a\",\"pointType\":\"temp\",\"unit\":\"F\"}}"
 
 # Sending Data
-r = requests.post(url = API_ENDPOINT, data = sensor)
+#r = requests.post(url = API_ENDPOINT, data = sensor,verify=False)
 
 # extracting response text 
+#pastebin_url = r.text
+#print("The pastebin URL is:%s"%pastebin_url)
+#a318e0a7-4c79-417c-ad8b-b1c96c55c453
+
+#The pastebin URL is:{"result":"SUCCESS","uuid":"0fd115a6-8228-4c35-9f4f-7144519d0a76"}
+
+#querying the Sensor just inserted.
+
+API_ENDPOINT_QUERY = "https://localhost:8080/api/query"
+
+query = "{\"query\":{\"uuid\":\"a318e0a7-4c79-417c-ad8b-b1c96c55c453\"}}";
+
+# Sending query
+r = requests.post(url = API_ENDPOINT_QUERY, data = query,verify=False)
+
 pastebin_url = r.text
 print("The pastebin URL is:%s"%pastebin_url)
 
-#The pastebin URL is:{"result":"SUCCESS","uuid":"0fd115a6-8228-4c35-9f4f-7144519d0a76"}
