f5cbd05e36edc4a78cb6852ad94b5fad696f0985
Sandeep Singh Sandha <sandha.iitr@gmail.com>
2017-12-04 12:42:25 -0800

    update

--- client/SendData.py
+++ client/SendData.py
@@ -11,11 +11,11 @@ API_ENDPOINT = "https://localhost:8080/api/data"
 data = "{\"userToken\":\"bd694827-8665-45c6-ad2e-720e15385934\",\"uuid\":\"9399222e-9fb9-40fb-bb6d-1ed0bad9a0f7\",\"data\":[{\"uuid\":\"0fd115a6-8228-4c35-9f4f-7144519d0a76\",\"timestamp\":1499813708623,\"value\":15,\"geometryType\":\"point\",\"coordinates\":[[30,60]]}]}"
 
 # Sending Data
-r = requests.post(url = API_ENDPOINT, data = data, verify=False)
+#r = requests.post(url = API_ENDPOINT, data = data, verify=False)
 
 # extracting response text 
-pastebin_url = r.text
-print("The pastebin URL is:%s"%pastebin_url)
+#pastebin_url = r.text
+#print("The pastebin URL is:%s"%pastebin_url)
 
 
 #query the data from the API
@@ -23,7 +23,7 @@ API_ENDPOINT_QUERY = "https://localhost:8080/api/querydata"
 
 
 #query to be send to api
-query="{\"query\":{\"lat_min\":29,\"lat_max\":31,\"lng_min\":59,\"lng_max\":61,\"timestamp_min\":1499813608623,\"timestamp_max\":1499813808623}}"
+query="{\"userToken\":\"bd694827-8665-45c6-ad2e-720e15385934\",\"uuid\":\"9399222e-9fb9-40fb-bb6d-1ed0bad9a0f7\",\"query\":{\"lat_min\":29,\"lat_max\":31,\"lng_min\":59,\"lng_max\":61,\"timestamp_min\":1499813608623,\"timestamp_max\":1499813808623}}"
 
 r = requests.post(url = API_ENDPOINT_QUERY, data = query, verify=False)
 
