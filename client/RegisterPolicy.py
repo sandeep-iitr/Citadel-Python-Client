4a76055898b385076dbc487668a91d48a9e7be06
Sandeep Singh Sandha <sandha.iitr@gmail.com>
2017-12-17 15:04:13 -0800

    update

--- client/RegisterPolicy.py
+++ client/RegisterPolicy.py
@@ -12,8 +12,16 @@ import requests
 # defining the api-endpoint 
 API_ENDPOINT = "https://localhost:8089/api/registerPolicy"
 
-Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com","testcitadel3@gmail.com"]}}'
+#Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com","testcitadel3@gmail.com"]}}'
 
 # Sending Data
+#r = requests.post(url = API_ENDPOINT, data = Policy,verify=False)
+#print("Response is:%s"%r.text)
+
+#Spatio-Temporal Policy
+#Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com"], "where":{"allowedPolygons":[[{"lat":30.0,"lng":60.0},{"lat":40.0,"lng":60.0},{"lat":40.0,"lng":70.0},{"lat":30.0,"lng":70.0},{"lat":30.0,"lng":60.0}],[{"lat":30.0,"lng":60.0},{"lat":40.0,"lng":60.0},{"lat":40.0,"lng":70.0},{"lat":30.0,"lng":70.0},{"lat":30.0,"lng":60.0}]]}}}'
+
+Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com"], "where":{"allowedPolygons":[[{"lat":30.0,"lng":60.0},{"lat":40.0,"lng":60.0},{"lat":40.0,"lng":70.0},{"lat":30.0,"lng":70.0},{"lat":30.0,"lng":60.0}],[{"lat":5.0,"lng":5.0},{"lat":50.0,"lng":5.0},{"lat":50.0,"lng":70.0},{"lat":5.0,"lng":70.0},{"lat":5.0,"lng":5.0}]]}}}'
+
 r = requests.post(url = API_ENDPOINT, data = Policy,verify=False)
 print("Response is:%s"%r.text)
--- client/SendData.py
+++ client/SendData.py
@@ -8,12 +8,12 @@ import requests
 API_ENDPOINT = "https://localhost:8080/api/data"
 
 # data to be sent to api
-data = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","uuid":"ee548c8c-5636-452d-b0a7-57fb4b6abeef","data":[{"timestamp":1499813708623,"value":15,"geometryType":"point","coordinates":[[30,60]]}]}'
+data = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","uuid":"ee548c8c-5636-452d-b0a7-57fb4b6abeef","data":[{"timestamp":1499813708623,"value":15,"geometryType":"point","coordinates":[[10,10]]}]}'
 
 # Sending Data
-#r = requests.post(url = API_ENDPOINT, data = data, verify=False)
+r = requests.post(url = API_ENDPOINT, data = data, verify=False)
 
-#print("Response is:%s"%r.text)
+print("Response is:%s"%r.text)
 
 
 #query the data from the API
@@ -25,7 +25,8 @@ API_ENDPOINT_QUERY = "https://localhost:8080/api/querydata"
 
 #query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","uuid":"ee548c8c-5636-452d-b0a7-57fb4b6abeef","query":{"lat_min":29,"lat_max":31,"lng_min":59,"lng_max":61,"timestamp_min":1499813608623,"timestamp_max":1499813808623}}'
 
-query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5YWVmYjgxNS0zNDkyLTQ1NTYtOGNlMy1lNTNiNzdiN2E4NzAifQ.9xeIUQPmt_5XI1i_Nmbp5oz2YrZAb2sLEU9VLkszQ4o","uuid":"ee548c8c-5636-452d-b0a7-57fb4b6abeef","query":{"lat_min":29,"lat_max":31,"lng_min":59,"lng_max":61,"timestamp_min":1499813608623,"timestamp_max":1499813808623}}'
+#query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5YWVmYjgxNS0zNDkyLTQ1NTYtOGNlMy1lNTNiNzdiN2E4NzAifQ.9xeIUQPmt_5XI1i_Nmbp5oz2YrZAb2sLEU9VLkszQ4o","uuid":"ee548c8c-5636-452d-b0a7-57fb4b6abeef","query":{"lat_min":29,"lat_max":31,"lng_min":59,"lng_max":61,"timestamp_min":1499813608623,"timestamp_max":1499813808623}}'
+query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5YWVmYjgxNS0zNDkyLTQ1NTYtOGNlMy1lNTNiNzdiN2E4NzAifQ.9xeIUQPmt_5XI1i_Nmbp5oz2YrZAb2sLEU9VLkszQ4o","uuid":"ee548c8c-5636-452d-b0a7-57fb4b6abeef","query":{"lat_min":10,"lat_max":90,"lng_min":10,"lng_max":90,"timestamp_min":1499813608623,"timestamp_max":1499813808623}}'
 
 
 r = requests.post(url = API_ENDPOINT_QUERY, data = query, verify=False)
