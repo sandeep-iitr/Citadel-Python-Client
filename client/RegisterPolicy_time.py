f1a54746fd5be2f441dcdc4456414e69c951a5eb
Sandeep Singh Sandha <sandha.iitr@gmail.com>
2017-12-17 19:25:06 -0800

    update

--- client/RegisterPolicy_time.py
+++ client/RegisterPolicy_time.py
@@ -21,7 +21,10 @@ API_ENDPOINT = "https://localhost:8089/api/registerPolicy"
 #Spatio-Temporal Policy
 #Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com"], "where":{"allowedPolygons":[[{"lat":30.0,"lng":60.0},{"lat":40.0,"lng":60.0},{"lat":40.0,"lng":70.0},{"lat":30.0,"lng":70.0},{"lat":30.0,"lng":60.0}],[{"lat":30.0,"lng":60.0},{"lat":40.0,"lng":60.0},{"lat":40.0,"lng":70.0},{"lat":30.0,"lng":70.0},{"lat":30.0,"lng":60.0}]]}}}'
 
-Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com"], "when":{"allowTimes":[{"start":123,"end":156},{"start":1499813604623,"end":1499823609623}],"denyTimes":[{"start":1499813604623,"end":1499823609623}]} } }'
+#Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com"], "when":{"allowTimes":[{"start":123,"end":156},{"start":1499813604623,"end":1499823609623}],"denyTimes":[{"start":1499813604623,"end":1499823609623}]} } }'
+
+Policy = '{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMmNiOTljYS1lMTRkLTQ1OGMtOTAwOS00MjI4NGMwMTk4YTIifQ.DpPJ0nuh4EvAdEG28nWcwMkjMZ7KIlPLtQu1idRnyGU","policy":{"sensors":["ee548c8c-5636-452d-b0a7-57fb4b6abeef"], "users":["testcitadel1@gmail.com"], "when":{"allowTimes":[{"start":123,"end":156},{"start":1499813604623,"end":1499823609623}]} } }'
+
 
 r = requests.post(url = API_ENDPOINT, data = Policy,verify=False)
 print("Response is:%s"%r.text)
