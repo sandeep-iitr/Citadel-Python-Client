35b910de086f82f706926fb3e22624527b970d0f
Sandeep Singh Sandha <sandha.iitr@gmail.com>
2017-12-18 12:35:18 -0800

    update

--- client/new/QueryData.py
+++ client/new/QueryData.py
@@ -7,10 +7,10 @@ API_ENDPOINT_QUERY = "https://citadel.westus.cloudapp.azure.com:8080/api/queryda
 
 
 #Query by citadel.ucla@gmail.com
-query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkYmI2YWQ0My0yZDA2LTQ3OWEtOGE1Yy05NDJkZjM3YThjODgifQ.x-9hwjTm8JQ7tsqRTSb0G_qn1CWA9xZKhCYefdMvBHc","uuid":"302ce0e0-80d4-4b0e-8c3e-c76fb756d4be","query":{"lat_min":30,"lat_max":30.1,"lng_min":60,"lng_max":60.1,"timestamp_min":1451606400000,"timestamp_max":1463142400000}}'
+#query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkYmI2YWQ0My0yZDA2LTQ3OWEtOGE1Yy05NDJkZjM3YThjODgifQ.x-9hwjTm8JQ7tsqRTSb0G_qn1CWA9xZKhCYefdMvBHc","uuid":"302ce0e0-80d4-4b0e-8c3e-c76fb756d4be","query":{"lat_min":30,"lat_max":30.1,"lng_min":60,"lng_max":60.1,"timestamp_min":1451606400000,"timestamp_max":1463142400000}}'
 
 #Query by: testcitadel1
-#query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3YzgxYjlhZS1kYmJjLTRjZTYtYjE0ZC1kNjBjZWY5Mzc1MDcifQ.lePmyCrFhYA7MG2DkhtsC97Gbk784puPW4ghQit_QZY","uuid":"302ce0e0-80d4-4b0e-8c3e-c76fb756d4be","query":{"lat_min":30,"lat_max":30.1,"lng_min":60,"lng_max":60.1,"timestamp_min":1451606400000,"timestamp_max":1463142400000}}'
+query='{"userToken":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxNTNlNjdmOS0zMTBiLTQ4NGUtODA4NS00NzcyY2YyMWU3ZWYifQ.nyMBn3wYuyimjYfgEivS4g-hni14ilpnPuigHU5Mp0M","uuid":"302ce0e0-80d4-4b0e-8c3e-c76fb756d4be","query":{"lat_min":30,"lat_max":30.1,"lng_min":60,"lng_max":60.1,"timestamp_min":1451606400000,"timestamp_max":1463142400000}}'
 
 
 r = requests.post(url = API_ENDPOINT_QUERY, data = query, verify=False)
